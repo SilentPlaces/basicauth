60620266a3a5eccc86f091bd52e61fa1eed4a972
Armin <arminaray@gmail.com>
2025-03-21 16:32:42 +0330

    removed json notation from user entity

--- internal/models/user/user.go
+++ internal/models/user/user.go
@@ -4,10 +4,10 @@ import "time"
 
 type (
 	User struct {
-		ID        string    `json:"id"`
-		Name      string    `json:"name"`
-		Email     string    `json:"email"`
-		Password  string    `json:"-"`
-		CreatedAt time.Time `json:"-"`
+		ID        string
+		Name      string
+		Email     string
+		Password  string
+		CreatedAt time.Time
 	}
 )
